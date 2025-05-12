aac7e4f74bb24d8e82e8b84a1097ec91c4a027ee
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-05-13 03:18:48 +0500

    Added Cart Schema

---
+++ src/models/Cart.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+  product_id: {
+    type: String, 
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  image: {
+    type: String
+  }
+});
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  session_id: {
+    type: String
+  },
+  items: [cartItemSchema],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+export default Cart;
