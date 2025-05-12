c185ca00d1ba9c39ae86eb647fe6f96c9aa0fa82
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-05-13 03:19:39 +0500

    Added Review Schema

---
+++ src/models/Review.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+  product_id: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  title: {
+    type: String
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+export default Review;
