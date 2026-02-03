42ca0dd9a97267c34b68e8c7dc09e7a6bca6b952
abdulraheem5335 <abdurraheem5335@gmail.com>
2026-02-03 21:10:44 +0500

    Deployment Settings

--- server.js
+++ server.js
@@ -56,6 +56,10 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
